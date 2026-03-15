656e821f8a035d823e853338760be3c644b66c15
kiyoto0722 <0wk38d0w2gf5z5y@gmail.com>
2026-03-15 22:11:14 +0900

    Update script.js

--- script.js
+++ script.js
@@ -39,6 +39,63 @@ const prefs = [
 
 ];
 
+const prefJP = {
+hokkaido:"北海道",
+aomori:"青森",
+iwate:"岩手",
+miyagi:"宮城",
+akita:"秋田",
+yamagata:"山形",
+fukushima:"福島",
+
+ibaraki:"茨城",
+tochigi:"栃木",
+gunma:"群馬",
+saitama:"埼玉",
+chiba:"千葉",
+tokyo:"東京",
+kanagawa:"神奈川",
+
+niigata:"新潟",
+toyama:"富山",
+ishikawa:"石川",
+fukui:"福井",
+yamanashi:"山梨",
+nagano:"長野",
+
+gifu:"岐阜",
+shizuoka:"静岡",
+aichi:"愛知",
+mie:"三重",
+
+shiga:"滋賀",
+kyoto:"京都",
+osaka:"大阪",
+hyogo:"兵庫",
+nara:"奈良",
+wakayama:"和歌山",
+
+tottori:"鳥取",
+shimane:"島根",
+okayama:"岡山",
+hiroshima:"広島",
+yamaguchi:"山口",
+
+tokushima:"徳島",
+kagawa:"香川",
+ehime:"愛媛",
+kochi:"高知",
+
+fukuoka:"福岡",
+saga:"佐賀",
+nagasaki:"長崎",
+kumamoto:"熊本",
+oita:"大分",
+miyazaki:"宮崎",
+kagoshima:"鹿児島",
+okinawa:"沖縄"
+};
+
 function startGame(){
 
 questionCount = 0;
@@ -270,7 +327,7 @@ let modeText = document.getElementById("modeSelect").selectedOptions[0].text;
 let rank = "";
 
 if(modeText==="全国ランダム"){
-  modeText = "全国";
+  modeText = prefJP[randomPref];
 }
   
 if(correctCount===5){
